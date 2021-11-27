788f3d65160b6196260b55843f5fd151994fabbf
Christopher <programmerchristopher@gmail.com>
2021-11-26 23:48:56 -0800

    JUnit setup

---
+++ src/JUnit.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class  JUnit
+{
+	@Test
+    public void testGetTopologicalOrder() {
+        //STUB
+        
+		//assertEquals(expected, test);
+    }
+
+    @Test
+    public void testGetShortestPath() {
+        //STUB
+
+		//assertEquals(expected, test);
+    }
+
+    @Test
+    public void testGetCheapestPath() {
+        //STUB
+
+		//assertEquals(expected, test);
+    }
+}
