7e2f15df3ba5d089d574548ee7f32b3f6237fc15
setiwon <45044735+setiwon@users.noreply.github.com>
2021-11-29 18:39:57 -0800

    vertex source code from book

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,46 @@
-public final class Vertex<T> implements VertexInterface {
-    
-}
+class Vertex<T> implements VertexInterface<T>
+{
+   private T label;
+   private LinkedList<Edge> edgeList; // Edges to neighbors
+   private boolean visited;                          // True if visited
+   private VertexInterface<T> previousVertex;        // On path to this vertex
+   private double cost;                              // Of path to this vertex
+   
+   public Vertex(T vertexLabel)
+   {
+      label = vertexLabel;
+      edgeList = new LinkedList<>();
+      visited = false;
+      previousVertex = null;
+      cost = 0;
+//end constructor
+
+protected class Edge
+{
+   private VertexInterface<T> vertex; // Vertex at end of edge
+   private double weight;
+   
+   protected Edge(VertexInterface<T> endVertex, double edgeWeight)
+   {
+      vertex = endVertex;
+      weight = edgeWeight;
+   } // end constructor
+   
+   protected Edge(VertexInterface<T> endVertex)
+   {
+      vertex = endVertex;
+      weight = 0;
+   } // end constructor
+
+   protected VertexInterface<T> getEndVertex()
+   {
+      return vertex;
+   } // end getEndVertex
+   
+   protected double getWeight()
+   {
+      return weight; 
+   } // end getWeight
+} // end Edge
+
+ 
