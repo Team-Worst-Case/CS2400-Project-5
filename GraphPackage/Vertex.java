c0a11eb9923ea61060c38badbb53c6de31f1dbba
setiwon <45044735+setiwon@users.noreply.github.com>
2021-11-29 23:51:02 -0800

    format fixes

--- GraphPackage/Vertex.java
+++ GraphPackage/Vertex.java
@@ -138,23 +138,24 @@ public boolean connect(VertexInterface<T> endVertex, double edgeWeight)
   } // end NeighborIterator
 
 
-  public boolean hasNeighbor()
+public boolean hasNeighbor()
 {
 return !edgeList.isEmpty();
 } // end hasNeighbor
 
-  public VertexInterface<T> getUnvisitedNeighbor()
+public VertexInterface<T> getUnvisitedNeighbor()
   {
-  VertexInterface<T> result = null;
-  Iterator<VertexInterface<T>> neighbors = getNeighborIterator();
-  while ( neighbors.hasNext() && (result == null) )
-  {
-  VertexInterface<T> nextNeighbor = neighbors.next();
-  if (!nextNeighbor.isVisited())
-  result = nextNeighbor;
-  } // end while
-  return result;
-  } // end getUnvisitedNeighbor
+   VertexInterface<T> result = null;
+  
+    Iterator<VertexInterface<T>> neighbors = getNeighborIterator();
+    while ( neighbors.hasNext() && (result == null) )
+      {
+         VertexInterface<T> nextNeighbor = neighbors.next();
+         if (!nextNeighbor.isVisited())
+            result = nextNeighbor;
+      } // end while
+   return result;
+} // end getUnvisitedNeighbor
   
   @Override
   public void setPredecessor(VertexInterface<T> predecessor) {
