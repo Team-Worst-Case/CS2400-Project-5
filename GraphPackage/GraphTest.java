fea8e030ffee3d1b7dde55520d10538cf2d8d7ea
Christopher <programmerchristopher@gmail.com>
2021-12-01 21:44:27 -0800

    deleted unecessary files & got JUnit working

--- GraphPackage/GraphTest.java
+++ GraphPackage/GraphTest.java
@@ -6,54 +6,46 @@ public class GraphTest
 {
 	public static void main(String[] args)
 	{
-        //@Test
-        //public void testGraphs() {
-            System.out.println("\nstarted... \n");
+        System.out.println("\nstarted... \n");
 
-            GraphInterface<String> graphV = new Graph<>(5);
-            graphV.addEdge(3, 4);
-            graphV.addEdge(1, 3);
-            graphV.addEdge(3, 2);
-            graphV.addEdge(2, 1);
-            /*graphV.addEdge("A", "B");
-            graphV.addEdge("A", "D");
-            graphV.addEdge("A", "E");
-            graphV.addEdge("B", "E");
-            graphV.addEdge("D", "G");
-            graphV.addEdge("E", "F");
-            graphV.addEdge("E", "H");
-            graphV.addEdge("G", "H");
-            graphV.addEdge("H", "I");
-            graphV.addEdge("I", "F");
-            graphV.addEdge("F", "H");
-            graphV.addEdge("F", "C");
-            graphV.addEdge("C", "B");*/
+        GraphInterface<String> graphV = new Graph<>(5);
+        graphV.addEdge(3, 4);
+        graphV.addEdge(1, 3);
+        graphV.addEdge(3, 2);
+        graphV.addEdge(2, 1);
+        /*graphV.addEdge("A", "B");
+        graphV.addEdge("A", "D");
+        graphV.addEdge("A", "E");
+        graphV.addEdge("B", "E");
+        graphV.addEdge("D", "G");
+        graphV.addEdge("E", "F");
+        graphV.addEdge("E", "H");
+        graphV.addEdge("G", "H");
+        graphV.addEdge("H", "I");
+        graphV.addEdge("I", "F");
+        graphV.addEdge("F", "H");
+        graphV.addEdge("F", "C");
+        graphV.addEdge("C", "B");*/
 
-            System.out.println("Breadth-first traversal of graph with A as starting vertex:");
-            //System.out.println(graphV.getBreadthFirstTraversal("A"));
+        System.out.println("Breadth-first traversal of graph with A as starting vertex:");
+        //System.out.println(graphV.getBreadthFirstTraversal("A"));
+    }
 
-            //assertEquals("A", "A");
-        //}
+    public void testGetTopologicalOrder() {
+        //STUB
+        
+        //assertEquals(expected, test);
+    }
 
-        /*@Test
-        public void testGetTopologicalOrder() {
-            //STUB
-            
-            //assertEquals(expected, test);
-        }
+    public void testGetShortestPath() {
+        //STUB
 
-        @Test
-        public void testGetShortestPath() {
-            //STUB
+        //assertEquals(expected, test);
+    }
 
-            //assertEquals(expected, test);
-        }
+    public void testGetCheapestPath() {
+        //STUB
 
-        @Test
-        public void testGetCheapestPath() {
-            //STUB
-
-            //assertEquals(expected, test);
-        }*/
+        //assertEquals(expected, test);
     }
 }
